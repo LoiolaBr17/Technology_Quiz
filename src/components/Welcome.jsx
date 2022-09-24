3afca322ffbdb4cec764ab949d875518af3c63d5
LoiolaBr17 <mateustaua16@gmail.com>
2022-09-24 09:03:58 -0300

    criando o componente welcome

---
+++ src/components/Welcome.jsx
@@ -0,0 +1,17 @@
+import Quiz from '../img/quiz.svg'
+
+import './Welcome.css'
+
+const Welcome = () => {
+
+    return (
+        <div id='welcome'>
+            <h2>Seja bem vindo</h2>
+            <p>Clique o botão abaixo para começar:</p>
+            <button>Iniciar</button>
+            <img src={Quiz} alt="Inicio do Quiz" />
+        </div>
+    )
+}
+
+export default Welcome
