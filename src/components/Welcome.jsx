1eaedf61b1dddb2a04371395e98f69b1f8dd550c
LoiolaBr17 <mateustaua16@gmail.com>
2022-09-24 09:31:45 -0300

    criando o context quiz

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,8 +1,13 @@
+import { useContext } from 'react';
+import { QuizContext } from '../context/quiz';
+
 import Quiz from '../img/quiz.svg'
 
 import './Welcome.css'
 
-const Welcome = () => {
+const Welcome = ()  => {
+    const quizState = useContext(QuizContext)
+    console.log(quizState)
 
     return (
         <div id='welcome'>
